c3f5fac64599709ca601c50157f6e0c7a57bf6ca
Samuel Davis <samueljakdavis@gmail.com>
2024-06-05 23:16:28 -0400

    cleanup code style; rely on existing enums, provide debug output

--- src/random_enchants.cpp
+++ src/random_enchants.cpp
@@ -1,159 +1,151 @@
 #include "random_enchants.h"
 
-void rollPossibleEnchant(Player* player, Item* item)
+void attemptToApplyRandomEnchantment(std::string event, Player *player, Item *item)
 {
-    // Check global enable option
-    if (!sConfigMgr->GetOption<bool>("RandomEnchants.Enable", true))
-    {
+    const uint8 MAX_ENCHANT_CHANCES = 3;
+    bool isEnabled = sConfigMgr->GetOption<bool>("RandomEnchants.Enable", true) && sConfigMgr->GetOption<bool>(event, true);
+    if (!isEnabled)
         return;
-    }
 
-    uint32 itemQuality = item->GetTemplate()->Quality;
-    uint32 itemClass = item->GetTemplate()->Class;
+    bool debugIsEnabled = sConfigMgr->GetOption<bool>("RandomEnchants.Debug", false);
 
-    /* eliminates enchanting anything that isn't a recognized quality */
-    /* eliminates enchanting anything but weapons/armor */
-    if ((itemQuality > 5 || itemQuality < 1) || (itemClass != 2 && itemClass != 4))
-    {
+    ChatHandler chatHandle = ChatHandler(player->GetSession());
+    const ItemTemplate *itemTemplate = item->GetTemplate();
+    uint32 itemId = itemTemplate->ItemId;
+    uint32 itemQuality = itemTemplate->Quality;
+    uint32 itemClass = itemTemplate->Class;
+    uint32 itemSubClass = itemTemplate->SubClass;
+    uint32 itemLevel = itemTemplate->ItemLevel; // use for rarity?
+    std::string itemClassString = getItemClassString(itemClass);
+    std::string itemName = itemTemplate->Name1;
+    uint8 enchantmentSlotIterator = PROP_ENCHANTMENT_SLOT_0;
+
+    if (debugIsEnabled)
+        chatHandle.PSendSysMessage(
+            "{\n  name: %s,\n  id: %i,\n  quality: %i,\n  class: %i,\n  subClass: %i,\n  classString: %s,\n  level: %i\n}",
+            itemName, itemId, itemQuality, itemClass, itemSubClass, itemClassString == "" ? "missing" : itemClassString, itemLevel);
+
+    if (itemClassString == "")
         return;
-    }
 
-    int slotRand[3] = { -1, -1, -1 };
-    uint32 slotEnch[3] = { 0, 1, 5 };
+    float enchantChanceThresholds[MAX_ENCHANT_CHANCES] = {
+        sConfigMgr->GetOption<float>("RandomEnchants.EnchantChance1", 70.0f),
+        sConfigMgr->GetOption<float>("RandomEnchants.EnchantChance2", 65.0f),
+        sConfigMgr->GetOption<float>("RandomEnchants.EnchantChance3", 60.0f),
+    };
 
-    // Fetching the configuration values as float
-    float enchantChance1 = sConfigMgr->GetOption<float>("RandomEnchants.EnchantChance1", 70.0f);
-    float enchantChance2 = sConfigMgr->GetOption<float>("RandomEnchants.EnchantChance2", 65.0f);
-    float enchantChance3 = sConfigMgr->GetOption<float>("RandomEnchants.EnchantChance3", 60.0f);
-
-    if (rand_chance() < enchantChance1)
-        slotRand[0] = getRandEnchantment(item);
-    if (slotRand[0] != -1 && rand_chance() < enchantChance2)
-        slotRand[1] = getRandEnchantment(item);
-    if (slotRand[1] != -1 && rand_chance() < enchantChance3)
-        slotRand[2] = getRandEnchantment(item);
-
-    for (int i = 0; i < 3; i++)
+    size_t i = 0;
+    for (; i < MAX_ENCHANT_CHANCES; i++)
     {
-        if (slotRand[i] != -1)
-        {
-            if (sSpellItemEnchantmentStore.LookupEntry(slotRand[i]))
-            {   //Make sure enchantment id exists
-                player->ApplyEnchantment(item, EnchantmentSlot(slotEnch[i]), false);
-                item->SetEnchantment(EnchantmentSlot(slotEnch[i]), slotRand[i], 0, 0);
-                player->ApplyEnchantment(item, EnchantmentSlot(slotEnch[i]), true);
-            }
-        }
+        double chance = rand_chance();
+        float threshold = enchantChanceThresholds[i];
+        int32 itemRarity = getRandomItemRarity(itemQuality);
+        int8 itemTier = getItemTier(itemRarity);
+        int32 enchantment = getEnchantment(itemTier, itemClassString, itemSubClass);
+        bool enchantmentExists = sSpellItemEnchantmentStore.LookupEntry(enchantment);
+        for (; enchantmentSlotIterator < MAX_ENCHANTMENT_SLOT; ++enchantmentSlotIterator)
+            if (!item->GetEnchantmentId(EnchantmentSlot(enchantmentSlotIterator)))
+                break;
+
+        if (debugIsEnabled)
+            chatHandle.PSendSysMessage(
+                "{\n  iteration: %i,\n  chance: %lf,\n  threshold: %f rarity: %i,\n  tier: %i,\n  enchantment: %i,\n  exists: %i,\n  slot: %i\n}",
+                i, chance, threshold, itemRarity, itemTier, enchantment, enchantmentExists ? 1 : 0, enchantmentSlotIterator);
+
+        if (chance > threshold || itemRarity == -1 || itemTier == -1 || enchantment == -1 || !enchantmentExists || enchantmentSlotIterator >= MAX_ENCHANTMENT_SLOT)
+            break;
+
+        EnchantmentSlot enchantmentSlot = EnchantmentSlot(enchantmentSlotIterator);
+        player->ApplyEnchantment(item, enchantmentSlot, false);
+        item->SetEnchantment(enchantmentSlot, enchantment, 0, 0);
+        player->ApplyEnchantment(item, enchantmentSlot, true);
     }
 
-    ChatHandler chathandle = ChatHandler(player->GetSession());
-
-    uint8 loc_idx = player->GetSession()->GetSessionDbLocaleIndex();
-    const ItemTemplate* temp = item->GetTemplate();
-    std::string name = temp->Name1;
-    if (ItemLocale const* il = sObjectMgr->GetItemLocale(temp->ItemId))
-        ObjectMgr::GetLocaleString(il->Name, loc_idx, name);
-
-
-    if (slotRand[2] != -1)
-        chathandle.PSendSysMessage("Newly Acquired |cffFF0000 %s |rhas received|cffFF0000 3 |rrandom enchantments!", name);
-    else if (slotRand[1] != -1)
-        chathandle.PSendSysMessage("Newly Acquired |cffFF0000 %s |rhas received|cffFF0000 2 |rrandom enchantments!", name);
-    else if (slotRand[0] != -1)
-        chathandle.PSendSysMessage("Newly Acquired |cffFF0000 %s |rhas received|cffFF0000 1 |rrandom enchantment!", name);
+    chatHandle.PSendSysMessage("Newly Acquired |cffFF0000 %s |rhas received|cffFF0000 %i |rrandom enchantments!", itemName, i);
 }
 
-uint32 getRandEnchantment(Item* item)
+std::string getItemClassString(uint32 itemClass)
 {
-    uint32 itemClass = item->GetTemplate()->Class;
-    uint32 itemQuality = item->GetTemplate()->Quality;
-    std::string classQueryString = "";
-    int rarityRoll = -1;
-    uint8 tier = 0;
+    if (itemClass == ITEM_CLASS_WEAPON)
+        return "WEAPON";
+    if (itemClass == ITEM_CLASS_ARMOR)
+        return "ARMOR";
+    return "";
+}
 
-    switch (itemClass)
-    {
-        case 2:
-            classQueryString = "WEAPON";
-            break;
-        case 4:
-            classQueryString = "ARMOR";
-            break;
-    }
-
-    if (classQueryString == "")
-        return -1;
-
+int32 getRandomItemRarity(uint32 itemQuality)
+{
+    uint32 chance = rand_norm();
     switch (itemQuality)
     {
-        case GREY:
-            rarityRoll = rand_norm() * 25;
-            break;
-        case WHITE:
-            rarityRoll = rand_norm() * 50;
-            break;
-        case GREEN:
-            rarityRoll = 45 + (rand_norm() * 20);
-            break;
-        case BLUE:
-            rarityRoll = 65 + (rand_norm() * 15);
-            break;
-        case PURPLE:
-            rarityRoll = 80 + (rand_norm() * 14);
-            break;
-        case ORANGE:
-            rarityRoll = 93;
-            break;
-    }
-
-    if (rarityRoll < 0)
+    case ITEM_QUALITY_POOR:
+        return chance * 25;
+    case ITEM_QUALITY_NORMAL:
+        return chance * 50;
+    case ITEM_QUALITY_UNCOMMON:
+        return 45 + chance * 20;
+    case ITEM_QUALITY_RARE:
+        return 65 + chance * 15;
+    case ITEM_QUALITY_EPIC:
+        return 80 + chance * 14;
+    case ITEM_QUALITY_LEGENDARY:
+        return 93;
+    default:
         return -1;
-
-    if (rarityRoll <= 44)
-        tier = 1;
-    else if (rarityRoll <= 64)
-        tier = 2;
-    else if (rarityRoll <= 79)
-        tier = 3;
-    else if (rarityRoll <= 92)
-        tier = 4;
-    else
-        tier = 5;
-
-    QueryResult result = WorldDatabase.Query("SELECT `enchantID` FROM `item_enchantment_random_tiers` WHERE `tier`={} AND `exclusiveSubClass`=NULL AND exclusiveSubClass='{}' OR `class`='{}' OR `class`='ANY' ORDER BY RAND() LIMIT 1", tier, item->GetTemplate()->SubClass, classQueryString, classQueryString);
-
-    if (!result)
-        return 0;
-
-    return result->Fetch()[0].Get<uint32>();
+    }
 }
 
-void RandomEnchantsPlayer::OnLogin(Player* player)
+int8 getItemTier(int32 itemRarity)
+{
+    if (itemRarity > 92)
+        return 5;
+    if (itemRarity > 79)
+        return 4;
+    if (itemRarity > 64)
+        return 3;
+    if (itemRarity > 44)
+        return 2;
+    if (itemRarity >= 0)
+        return 1;
+    return -1;
+}
+
+int32 getEnchantment(int32 tier, std::string classString, uint32 exclusiveSubClass)
+{
+    std::string query = "SELECT `enchantID`"
+                        " FROM `item_enchantment_random_tiers`"
+                        " WHERE `tier`={}"
+                        " AND (`class`='ANY' OR `class`='{}')"
+                        " AND (`exclusiveSubClass` IS NULL OR `exclusiveSubClass`={})"
+                        " ORDER BY RAND()"
+                        " LIMIT 1";
+    QueryResult result = WorldDatabase.Query(query, tier, classString, exclusiveSubClass);
+
+    return result ? result->Fetch()[0].Get<uint32>() : -1;
+}
+
+void RandomEnchantsPlayer::OnLogin(Player *player)
 {
     if (sConfigMgr->GetOption<bool>("RandomEnchants.AnnounceOnLogin", true) && (sConfigMgr->GetOption<bool>("RandomEnchants.Enable", true)))
         ChatHandler(player->GetSession()).SendSysMessage(sConfigMgr->GetOption<std::string>("RandomEnchants.OnLoginMessage", "This server is running a RandomEnchants Module.").c_str());
 }
 
-void RandomEnchantsPlayer::OnLootItem(Player* player, Item* item, uint32 /*count*/, ObjectGuid /*lootguid*/)
+void RandomEnchantsPlayer::OnLootItem(Player *player, Item *item, uint32 /*count*/, ObjectGuid /*lootguid*/)
 {
-    if (sConfigMgr->GetOption<bool>("RandomEnchants.OnLoot", true) && sConfigMgr->GetOption<bool>("RandomEnchants.Enable", true))
-        rollPossibleEnchant(player, item);
+    attemptToApplyRandomEnchantment("RandomEnchants.OnLoot", player, item);
 }
 
-void RandomEnchantsPlayer::OnCreateItem(Player* player, Item* item, uint32 /*count*/)
+void RandomEnchantsPlayer::OnCreateItem(Player *player, Item *item, uint32 /*count*/)
 {
-    if (sConfigMgr->GetOption<bool>("RandomEnchants.OnCreate", true) && (sConfigMgr->GetOption<bool>("RandomEnchants.Enable", true)))
-        rollPossibleEnchant(player, item);
+    attemptToApplyRandomEnchantment("RandomEnchants.OnCreate", player, item);
 }
 
-void RandomEnchantsPlayer::OnQuestRewardItem(Player* player, Item* item, uint32 /*count*/)
+void RandomEnchantsPlayer::OnQuestRewardItem(Player *player, Item *item, uint32 /*count*/)
 {
-    if (sConfigMgr->GetOption<bool>("RandomEnchants.OnQuestReward", true) && (sConfigMgr->GetOption<bool>("RandomEnchants.Enable", true)))
-        rollPossibleEnchant(player, item);
+    attemptToApplyRandomEnchantment("RandomEnchants.OnQuestReward", player, item);
 }
 
-void RandomEnchantsPlayer::OnGroupRollRewardItem(Player* player, Item* item, uint32 /*count*/, RollVote /*voteType*/, Roll* /*roll*/)
+void RandomEnchantsPlayer::OnGroupRollRewardItem(Player *player, Item *item, uint32 /*count*/, RollVote /*voteType*/, Roll * /*roll*/)
 {
-    if (sConfigMgr->GetOption<bool>("RandomEnchants.OnGroupRoll", true) && (sConfigMgr->GetOption<bool>("RandomEnchants.Enable", true)))
-        rollPossibleEnchant(player, item);
+    attemptToApplyRandomEnchantment("RandomEnchants.OnGroupRoll", player, item);
 }
--- src/random_enchants.h
+++ src/random_enchants.h
@@ -1,35 +1,28 @@
 /*
-* Converted from the original LUA script to a module for Azerothcore (Sunwell)
-*/
+ * Converted from the original LUA script to a module for Azerothcore (Sunwell)
+ */
 
 #include "ScriptMgr.h"
 #include "Player.h"
 #include "Configuration/Config.h"
 #include "Chat.h"
 
-enum ItemQuality
-{
-    GREY = 0,
-    WHITE,
-    GREEN,
-    BLUE,
-    PURPLE,
-    ORANGE
-};
-
-void rollPossibleEnchant(Player* player, Item* item);
-[[nodiscard]] uint32 getRandEnchantment(Item* item);
+void attemptToApplyRandomEnchantment(std::string event, Player *player, Item *item);
+[[nodiscard]] std::string getItemClassString(uint32 itemClass);
+[[nodiscard]] int32 getRandomItemRarity(uint32 itemQuality);
+[[nodiscard]] int8 getItemTier(int32 itemRarity);
+[[nodiscard]] int32 getEnchantment(int32 tier, std::string classString, uint32 exclusiveSubClass);
 
 class RandomEnchantsPlayer : public PlayerScript
 {
 public:
-    RandomEnchantsPlayer() : PlayerScript("RandomEnchantsPlayer") { }
+    RandomEnchantsPlayer() : PlayerScript("RandomEnchantsPlayer") {}
 
-    void OnLogin(Player* player) override;
-    void OnLootItem(Player* player, Item* item, uint32 /*count*/, ObjectGuid /*lootguid*/) override;
-    void OnCreateItem(Player* player, Item* item, uint32 /*count*/) override;
-    void OnQuestRewardItem(Player* player, Item* item, uint32 /*count*/) override;
-    void OnGroupRollRewardItem(Player* player, Item* item, uint32 /*count*/, RollVote /*voteType*/, Roll* /*roll*/) override;
+    void OnLogin(Player *player) override;
+    void OnLootItem(Player *player, Item *item, uint32 /*count*/, ObjectGuid /*lootguid*/) override;
+    void OnCreateItem(Player *player, Item *item, uint32 /*count*/) override;
+    void OnQuestRewardItem(Player *player, Item *item, uint32 /*count*/) override;
+    void OnGroupRollRewardItem(Player *player, Item *item, uint32 /*count*/, RollVote /*voteType*/, Roll * /*roll*/) override;
 };
 
 void AddRandomEnchantsScripts()
